73ff22584f1bcbe6aa53a9749b100632d7d0e234
Oğuzhan Can <59476485+OguzhanC07@users.noreply.github.com>
2021-03-17 17:25:02 +0300

    Added simple xunit test with moq for repositories

---
+++ FileManagement.Test/FileServiceTest.cs
@@ -0,0 +1,65 @@
+﻿using FileManagement.Business.Concrete;
+using FileManagement.DataAccess;
+using FileManagement.DataAccess.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FileManagement.Test
+{
+    public class FileServiceTest
+    {
+        private readonly FileManager _sut;
+        private readonly Mock<IGenericDal<File>> _fileRepoMock = new Mock<IGenericDal<File>>();
+
+        public FileServiceTest()
+        {
+            _sut = new FileManager(_fileRepoMock.Object);
+        }
+
+        [Fact]
+        public async Task GetFileById_ShouldReturnFile_WhenFileExists()
+        {
+            //Arrange
+            var random = new Random();
+            int fileId = random.Next(1, 100);
+            var fileName = "test filename";
+            var mockFile = new File
+            {
+                Id = fileId,
+                FileName = fileName,
+                FileGuid = Guid.NewGuid().ToString(),
+                Size = 500,
+                UploadedAt = DateTime.Now,
+                IsActive = true
+            };
+            _fileRepoMock.Setup(x => x.GetById(fileId)).ReturnsAsync(mockFile);
+
+            //Act
+            var file = await _sut.GetById(fileId);
+
+            //Assert
+            Assert.Equal(fileId, file.Id);
+            Assert.Equal(fileName, mockFile.FileName);
+        }
+
+        [Fact]
+        public async Task GetFileById_ShouldReturnNothing_WhenFileDoesNotExists()
+        {
+            //Arrange
+            var random = new Random();
+            int fileId = random.Next(1, 100);
+            _fileRepoMock.Setup(x => x.GetById(It.IsAny<int>())).ReturnsAsync(()=>null);
+
+            //Act
+            var file = await _sut.GetById(fileId);
+
+            //Assert
+            Assert.Null(file);
+        }
+    }
+}
---
+++ FileManagement.Test/FolderServiceTest.cs
@@ -0,0 +1,130 @@
+﻿using FileManagement.Business.Concrete;
+using FileManagement.Business.Interfaces;
+using FileManagement.DataAccess;
+using FileManagement.DataAccess.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FileManagement.Test
+{
+    public class FolderServiceTest
+    {
+        private readonly FolderManager _sut;
+        private readonly Mock<IGenericDal<Folder>> _folderRepoMock = new Mock<IGenericDal<Folder>>();
+        private readonly Mock<IFolderDal> _folderDalMock = new Mock<IFolderDal>();
+        public FolderServiceTest()
+        {
+            _sut = new FolderManager(_folderDalMock.Object, _folderRepoMock.Object);
+        }
+
+        [Fact]
+        public async Task GetFolderById_ShouldReturnFolder_WhenFolderExist()
+        {
+            var random = new Random();
+            int folderId = random.Next(1, 100);
+            var folderName = "test filename";
+
+            var mockFolder = new Folder
+            {
+                Id = folderId,
+                FolderName = folderName,
+                FileGuid = Guid.NewGuid(),
+                Size = 500,
+                CreatedAt = DateTime.Now,
+                IsDeleted = false,
+                AppUserId = 1,
+            };
+
+            _folderRepoMock.Setup(x => x.GetById(folderId)).ReturnsAsync(mockFolder);
+
+            //Act
+            var folder = await _sut.GetById(folderId);
+
+            //Assert
+            Assert.Equal(folderId, folder.Id);
+            Assert.Equal(folderName, mockFolder.FolderName);
+        }
+
+
+        [Fact]
+        public async Task GetFoldersByAppUserId_ShouldReturnListOfFiles_WhenUserExist()
+        {
+            //Arrange
+            Mock<IFolderService> folderServiceMock = new Mock<IFolderService>();
+            folderServiceMock.Setup(x => x.GetFoldersByUserId(1)).ReturnsAsync(GetSampleFolder());
+            var expected = GetSampleFolder();
+            //Act
+            var actual = await _sut.GetFoldersByUserId(1);
+            Console.WriteLine(actual);
+            //Assert 
+
+            Assert.Equal(expected.Count, actual.Count);
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i].FolderName, actual[i].FolderName);
+                Assert.Equal(expected[i].Size, actual[i].Size);
+            }
+
+        }
+
+        private List<Folder> GetSampleFolder()
+        {
+            List<Folder> output = new List<Folder>
+            {
+                new Folder
+                {
+                    Id=1,
+                    FolderName="test",
+                    CreatedAt=DateTime.Now,
+                    Size=500,
+                    FileGuid=Guid.NewGuid(),
+                    AppUserId=1,
+                    IsDeleted=false,
+                    ParentFolderId=null
+                },
+                new Folder
+                {
+                    Id=2,
+                    FolderName="test2",
+                    CreatedAt=DateTime.Now.AddSeconds(1),
+                    Size=600,
+                    FileGuid=Guid.NewGuid(),
+                    AppUserId=1,
+                    IsDeleted=false,
+                    ParentFolderId=1
+                },
+                new Folder
+                {
+                    Id=3,
+                    FolderName="test3",
+                    CreatedAt=DateTime.Now.AddSeconds(2),
+                    Size=700,
+                    FileGuid=Guid.NewGuid(),
+                    AppUserId=1,
+                    IsDeleted=false,
+                    ParentFolderId=null
+                },
+                new Folder
+                {
+                    Id=4,
+                    FolderName="test4",
+                    CreatedAt=DateTime.Now,
+                    Size=800,
+                    FileGuid=Guid.NewGuid(),
+                    AppUserId=1,
+                    IsDeleted=false,
+                    ParentFolderId=null
+                },
+            };
+
+            return output;
+        }
+
+    }
+}
